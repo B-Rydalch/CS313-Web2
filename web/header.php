f8b94e29aa52bb8453b75ca1bc5d1c422272adeb
B-Rydalch <bradrydalch@gmail.com>
2018-09-29 22:16:57 -0600

    update header

--- web/header.php
+++ web/header.php
@@ -1,22 +1,18 @@
 <?php
-    $filename = $_SERVER['REQUEST_URI'];
-    $about = "";
-    $home = "";
-    if ($filename == '/aboutme.php') {
-        $about = " active";
-    } else if ($filename == '/home.php') {
-        $home = " active";
-    echo "<nav class='navbar navbar-expand-md navbar-dark bg-dark fixed-top link-top'>
-            <a class='navbar-brand' href='#'>Brad Rydalch</a>
-            <div class='collapse navbar-collapse'>
-                <ul class='navbar-nav mr-auto'>
-                    <li class='nav-item'>
-                        <a class='nav-link$about' href='./about-us.php'>About us</a>
-                    </li>
-                    <li class='nav-item'>
-                        <a class='nav-link$home' href='./home.php'>Home</a>
-                    </li>
-                </ul>
-            </div>
-        </nav>";
+    $file = pathinfo($_SERVER['PHP_SELF'], PATHINFO_FILENAME);
 ?>
+
+<nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top link-top">
+    <a class="navbar-brand" href="#">Brad Rydalch</a>
+
+    <div class="collapse navbar-collapse">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item activef">
+                <a class="nav-link" href="#home">Home</a>
+            </li>
+            <li class="nav-item ">
+                <a class="nav-link" href="#projects">Projects</a>
+            </li>
+        </ul>
+    </div>
+</nav>
