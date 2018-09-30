d6502a313d4353a5eeefe65854704e9688744bc8
B-Rydalch <bradrydalch@gmail.com>
2018-09-29 22:24:01 -0600

    playground

--- web/index.php
+++ web/index.php
@@ -1,159 +1,157 @@
-<?php
-  <!doctype html>
-  <html lang="en">
-  
-  <head>
-      <meta charset="utf-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-      <meta name="description" content="">
-      <meta name="author" content="">
-      <link rel="shortcut icon" type="image/png" href="https://sylvanson.com/wp-content/uploads/2017/03/favicon-512.png">
-  
-      <title>Sam McGrath</title>
-  
-      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
-          crossorigin="anonymous">
-      <link href="main.css" rel="stylesheet">
-  </head>
-  
-  <body class="text-center">
-      <?php require 'header.php' ?>
-  
-      <div class="cover-container d-flex h-100 p-3 mx-auto flex-column">
-          <section class="intro">  
-              <a name="home"></a>
-              <div class="content">
-                  <h1 class="headerStyle">Sam McGrath</h1>
-  
-                  <div id="carousel1" class="carousel slide" data-ride="carousel" data-pause=false>
-                      <div class="carousel-inner">
-                          <div class="carousel-item active">
-                              <img src="./images/main/pic1-1.jpeg" alt="First slide">
-                              <div class="carousel-caption">
-                                  <p>Neuschwanstein Castle</p>
-                              </div>
-                          </div>
-                          <div class="carousel-item">
-                              <img src="./images/main/pic2-1.jpeg" alt="Second slide">
-                              <div class="carousel-caption">
-                                  <h1>Cairo, Egypt</h1>
-                              </div>
-                          </div>
-                          <div class="carousel-item">
-                              <img src="./images/main/pic3-1.jpeg" alt="Third slide">
-                              <div class="carousel-caption">
-                                  <p>Smithfield, Utah - Utah Ogden Mission</p>
-                              </div>
-                          </div>
-                      </div>
-                  </div>
-              </div>
-          </section>
-  
-          <section class="intro">  
-              <div class="content">
-                  <h1 class="headerStyle">Avid software developer, golfer and lover of the Office</h1>
-  
-                  <div id="carousel2" class="carousel slide" data-ride="carousel" data-pause=false>
-                      <div class="carousel-inner">
-                          <div class="carousel-item active">
-                              <img src="./images/main/pic4-1.jpeg" alt="First slide">
-                              <div class="carousel-caption">
-                                  <h1>Golf in Saudi Arabia</h1>
-                              </div>
-                          </div>
-                          <div class="carousel-item">
-                              <img src="./images/main/pic5-1.png" alt="Second slide">
-                              <div class="carousel-caption">
-                                  <h1>Jim and Dwight</h1>
-                              </div>
-                          </div>
-                      </div>
-                  </div>
-              </div>
-          </section>
-  
-          <section class="intro">
-              <a name="projects"></a>  
-              <div class="content">
-                  <h1 class="headerStyle projectHeader">Projects</h1>
-  
-                  <div class="card-deck">
-                      <div class="card mb-4">
-                          <img src="./images/main/comingSoon.png" alt="Coming soon">
-                          <div class="card-body d-flex flex-column">
-                              <h5 class="card-title">W03 Shopping Cart</h5>
-                              <p class="card-text">
-                                  Interactive shopping cart - coming soon...
-                              </p>
-                              <a href="#" target="_blank" rel="noopener" class="btn btn-primary mt-auto">Visit
-                                  Page
-                              </a>
-                          </div>
-                      </div>
-                      <div class="card mb-4">
-                          <img src="./images/main/comingSoon.png" alt="Coming soon">
-                          <div class="card-body d-flex flex-column">
-                              <h5 class="card-title">W04 Assignment</h5>
-                              <p class="card-text">
-                                  Insert assignment description
-                              </p>
-                              <a href="#" target="_blank" rel="noopener" class="btn btn-primary mt-auto">Visit
-                                  Page
-                              </a>
-                          </div>
-                      </div>
-                      <div class="card mb-4">
-                          <img src="./images/main/comingSoon.png" alt="Coming soon">
-                          <div class="card-body d-flex flex-column">
-                              <h5 class="card-title">W05 Assignment</h5>
-                              <p class="card-text">
-                                  Insert assignment description
-                              </p>
-                              <a href="#" target="_blank" rel="noopener" class="btn btn-primary mt-auto">Visit
-                                  Page
-                              </a>
-                          </div>
-                      </div>
-                      <div class="card mb-4">
-                          <img src="./images/main/comingSoon.png" alt="Coming soon">
-                          <div class="card-body d-flex flex-column">
-                              <h5 class="card-title">W06 Assignment</h5>
-                              <p class="card-text">
-                                  Insert assignment description
-                              </p>
-                              <a href="#" target="_blank" rel="noopener" class="btn btn-primary mt-auto">Visit
-                                  Page
-                              </a>
-                          </div>
-                      </div>
-                      <div class="card mb-4">
-                          <img src="./images/main/comingSoon.png" alt="Coming soon">
-                          <div class="card-body d-flex flex-column">
-                              <h5 class="card-title">W07 Assignment</h5>
-                              <p class="card-text">
-                                  Insert assignment description
-                              </p>
-                              <a href="#" target="_blank" rel="noopener" class="btn btn-primary mt-auto">Visit
-                                  Page
-                              </a>
-                          </div>
-                      </div>
-                  </div>
-  
-              </div>
-          </section>
-      </div>
-  
-      <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-          crossorigin="anonymous"></script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
-          crossorigin="anonymous"></script>
-      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
-          crossorigin="anonymous"></script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-      <script src="scripts.js"></script>
-  </body>
-  
-  </html>
-?>
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" type="image/png" href="https://sylvanson.com/wp-content/uploads/2017/03/favicon-512.png">
+
+    <title>Sam McGrath</title>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
+        crossorigin="anonymous">
+    <link href="main.css" rel="stylesheet">
+</head>
+
+<body class="text-center">
+    <?php require 'header.php' ?>
+
+    <div class="cover-container d-flex h-100 p-3 mx-auto flex-column">
+        <section class="intro">  
+            <a name="home"></a>
+            <div class="content">
+                <h1 class="headerStyle">Sam McGrath</h1>
+
+                <div id="carousel1" class="carousel slide" data-ride="carousel" data-pause=false>
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="./images/main/pic1-1.jpeg" alt="First slide">
+                            <div class="carousel-caption">
+                                <p>Neuschwanstein Castle</p>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <img src="./images/main/pic2-1.jpeg" alt="Second slide">
+                            <div class="carousel-caption">
+                                <h1>Cairo, Egypt</h1>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <img src="./images/main/pic3-1.jpeg" alt="Third slide">
+                            <div class="carousel-caption">
+                                <p>Smithfield, Utah - Utah Ogden Mission</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="intro">  
+            <div class="content">
+                <h1 class="headerStyle">Avid software developer, golfer and lover of the Office</h1>
+
+                <div id="carousel2" class="carousel slide" data-ride="carousel" data-pause=false>
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="./images/main/pic4-1.jpeg" alt="First slide">
+                            <div class="carousel-caption">
+                                <h1>Golf in Saudi Arabia</h1>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <img src="./images/main/pic5-1.png" alt="Second slide">
+                            <div class="carousel-caption">
+                                <h1>Jim and Dwight</h1>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="intro">
+            <a name="projects"></a>  
+            <div class="content">
+                <h1 class="headerStyle projectHeader">Projects</h1>
+
+                <div class="card-deck">
+                    <div class="card mb-4">
+                        <img src="./images/main/comingSoon.png" alt="Coming soon">
+                        <div class="card-body d-flex flex-column">
+                            <h5 class="card-title">W03 Shopping Cart</h5>
+                            <p class="card-text">
+                                Interactive shopping cart - coming soon...
+                            </p>
+                            <a href="#" target="_blank" rel="noopener" class="btn btn-primary mt-auto">Visit
+                                Page
+                            </a>
+                        </div>
+                    </div>
+                    <div class="card mb-4">
+                        <img src="./images/main/comingSoon.png" alt="Coming soon">
+                        <div class="card-body d-flex flex-column">
+                            <h5 class="card-title">W04 Assignment</h5>
+                            <p class="card-text">
+                                Insert assignment description
+                            </p>
+                            <a href="#" target="_blank" rel="noopener" class="btn btn-primary mt-auto">Visit
+                                Page
+                            </a>
+                        </div>
+                    </div>
+                    <div class="card mb-4">
+                        <img src="./images/main/comingSoon.png" alt="Coming soon">
+                        <div class="card-body d-flex flex-column">
+                            <h5 class="card-title">W05 Assignment</h5>
+                            <p class="card-text">
+                                Insert assignment description
+                            </p>
+                            <a href="#" target="_blank" rel="noopener" class="btn btn-primary mt-auto">Visit
+                                Page
+                            </a>
+                        </div>
+                    </div>
+                    <div class="card mb-4">
+                        <img src="./images/main/comingSoon.png" alt="Coming soon">
+                        <div class="card-body d-flex flex-column">
+                            <h5 class="card-title">W06 Assignment</h5>
+                            <p class="card-text">
+                                Insert assignment description
+                            </p>
+                            <a href="#" target="_blank" rel="noopener" class="btn btn-primary mt-auto">Visit
+                                Page
+                            </a>
+                        </div>
+                    </div>
+                    <div class="card mb-4">
+                        <img src="./images/main/comingSoon.png" alt="Coming soon">
+                        <div class="card-body d-flex flex-column">
+                            <h5 class="card-title">W07 Assignment</h5>
+                            <p class="card-text">
+                                Insert assignment description
+                            </p>
+                            <a href="#" target="_blank" rel="noopener" class="btn btn-primary mt-auto">Visit
+                                Page
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </section>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+        crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+        crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <script src="scripts.js"></script>
+</body>
+
+</html>
