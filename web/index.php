40559e3ed0540a834c2b33921df8a446189489b9
B-Rydalch <bradrydalch@gmail.com>
2018-09-30 08:15:29 -0600

    update

--- web/index.php
+++ web/index.php
@@ -1,26 +1,28 @@
 <!doctype html>
 <html lang="en">
-
-<head>
+  <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
     <link rel="shortcut icon" type="image/png" href="https://sylvanson.com/wp-content/uploads/2017/03/favicon-512.png">
-
     <title>CS231 Web Engineering 2</title>
-
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
         crossorigin="anonymous">
     <link href="styles.css" rel="stylesheet">
-</head>
-
-<body class="text-center">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+        crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+        crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <script src="scripts.js"></script>
+  </head>
+  <body class="text-center">
     <?php require 'header.php' ?>
-
     <div class="cover-container d-flex h-100 p-3 mx-auto flex-column">
-        <section class="intro">  
-            <a name="home"></a>
+        <section class="welcome">  
             <div class="content">
               <h1 class="headerStyle">Welcome</h1>
             </div>
@@ -29,7 +31,6 @@
             <a name="projects"></a>  
             <div class="content">
                 <h1 class="headerStyle projectHeader">Projects</h1>
-
                 <div class="card-deck">
                     <div class="card mb-4">
                         <img src="./images/main/comingSoon.png" alt="Coming soon">
@@ -48,7 +49,7 @@
                         <div class="card-body d-flex flex-column">
                             <h5 class="card-title">W04 Assignment</h5>
                             <p class="card-text">
-                                Insert assignment description
+                                assignment description
                             </p>
                             <a href="#" target="_blank" rel="noopener" class="btn btn-primary mt-auto">Visit
                                 Page
@@ -96,15 +97,5 @@
             </div>
         </section>
     </div>
-
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-        crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
-        crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
-        crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-    <script src="scripts.js"></script>
-</body>
-
+  </body>
 </html>
