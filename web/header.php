1b20bda8735f231214af5ae0e25426cf984fc0c1
B-Rydalch <bradrydalch@gmail.com>
2018-09-29 22:06:56 -0600

    update

--- web/header.php
+++ web/header.php
@@ -2,16 +2,12 @@
     $filename = $_SERVER['REQUEST_URI'];
     $about = "";
     $home = "";
-    $login = "";
-    if ($filename == '/w02-toa/about-us.php') {
+    if ($filename == '/w02-toa/about-me.php') {
         $about = " active";
     } else if ($filename == '/w02-toa/home.php') {
         $home = " active";
-    } else if ($filename == '/w02-toa/login.php') {
-        $login = " active";
-    }
     echo "<nav class='navbar navbar-expand-md navbar-dark bg-dark fixed-top link-top'>
-            <a class='navbar-brand' href='#'>Ice Cream Corp.</a>
+            <a class='navbar-brand' href='#'>Brad Rydalch</a>
             <div class='collapse navbar-collapse'>
                 <ul class='navbar-nav mr-auto'>
                     <li class='nav-item'>
@@ -19,9 +15,6 @@
                     </li>
                     <li class='nav-item'>
                         <a class='nav-link$home' href='./home.php'>Home</a>
-                    </li>
-                    <li class='nav-item'>
-                        <a class='nav-link$loging' href='./login.php'>Login</a>
                     </li>
                 </ul>
             </div>
