df2d2c02d2d5b21ab4b714df83ca84a179ae0c7c
B-Rydalch <bradrydalch@gmail.com>
2018-09-30 21:56:13 -0600

    css update

--- web/index.php
+++ web/index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-    <link rel="shortcut icon" type="image/png" href="https://sylvanson.com/wp-content/uploads/2017/03/favicon-512.png">
+    <!-- <link rel="shortcut icon" type="image/png" href="https://sylvanson.com/wp-content/uploads/2017/03/favicon-512.png"> -->
     <title>CS231 Web Engineering 2</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
         crossorigin="anonymous">
@@ -21,7 +21,7 @@
   </head>
   <body class="text-center">
     <?php require 'header.php' ?>
-    <div class="cover-container d-flex h-100 p-3 mx-auto flex-column">
+    <div>
         <section class="welcome">  
             <div class="content">
               <h1 class="headerStyle">Welcome</h1>
