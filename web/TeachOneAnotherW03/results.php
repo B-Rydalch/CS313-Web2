814fe883cdeaae9928b79b896e168771c7ac9248
B-Rydalch <bradrydalch@gmail.com>
2018-10-02 16:25:55 -0600

    .

--- web/TeachOneAnotherW03/results.php
+++ web/TeachOneAnotherW03/results.php
@@ -5,6 +5,13 @@ Welcome <?php echo $_POST["name"]; ?><br>
 <a href="mailto:<?php echo $_POST["email"]; ?>"> Email <?php echo $_POST["email"]; ?></a><br>
 Your major is: <?php echo $_POST["major"];?><br>
 Your comment is: <?php echo $_POST["comment"];?><br>
+Countries visited:<br>
+
+<?php
+    foreach($_POST["continent"] as $selected) {
+        echo "<p>$selected</p><br>";
+    }
+?>  
 
 </body>
 </html>
