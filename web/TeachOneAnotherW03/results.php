7ab8d72cba3edda77ce5bdf09bdeb064fe3bc2d8
B-Rydalch <bradrydalch@gmail.com>
2018-10-02 16:09:15 -0600

    teach

---
+++ web/TeachOneAnotherW03/results.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+
+Welcome <?php echo $_POST["name"]; ?><br>
+Your email address is: <?php echo $_POST["email"]; ?>
+Your major is: <?php echo $_POST["major"];?>
+Your comment is: <?php echo $_POST["comment"];?>
+
+</body>
+</html>
