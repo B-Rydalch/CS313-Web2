072463049bbd7c7c8606981dec6b5b50187556b4
B-Rydalch <bradrydalch@gmail.com>
2018-10-01 22:02:22 -0600

    update

--- web/header.php
+++ web/header.php
@@ -7,13 +7,13 @@
     <div class="collapse navbar-collapse">
         <ul class="navbar-nav mr-auto">
             <li class="nav-item activef">
-                <a class="nav-link" href="#index">Home</a>
+                <a class="nav-link" href="./index.php">Home</a>
             </li>
             <li class="nav-item ">
-                <a class="nav-link" href="#aboutme">About Me</a>
+                <a class="nav-link" href="./aboutme.php">About Me</a>
             </li>
             <li class="nav-item ">
-                <a class="nav-link" href="#projects">Projects</a>
+                <a class="nav-link" href="./projects.php">Projects</a>
             </li>
         </ul>
     </div>
---
+++ web/projects.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">   
+   <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <title>CS313</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
+        crossorigin="anonymous">
+        <link rel="stylesheet" href="styles.css">
+        <script src="scripts.js"></script>
+   </head> 
+   <body onload = "startTimer()">
+        <?php include 'header.php' ?>
+        <div class="list-group">
+            <a href="#" class="list-group-item list-group-item-action active">
+                Week 3
+            </a>
+            <a href="#" class="list-group-item list-group-item-action">Week 4</a>
+            <a href="#" class="list-group-item list-group-item-action">Week 5</a>
+            <a href="#" class="list-group-item list-group-item-action">week 6</a>
+            <a href="#" class="list-group-item list-group-item-action disabled">Week 7</a>
+        </div>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+        crossorigin="anonymous"></script>
+   </body>
+</html>
