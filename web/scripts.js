81306a4b904af8351e972e2266c479e8f5758d34
B-Rydalch <bradrydalch@gmail.com>
2018-10-02 15:43:22 -0600

    .

--- web/scripts.js
+++ web/scripts.js
@@ -17,3 +17,5 @@ images[0] = "images/image1.jpg";
 images[1] = "images/image2.jpg";
 images[2] = "images/image3.jpg";
 images[3] = "images/image4.jpg";
+images[4] = "images/terror.jpg";
+images[5] = "images/fam.png";
