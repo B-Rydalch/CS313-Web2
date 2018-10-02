d4d4df59673f4879cb8d66b6dac704769bf8bb56
B-Rydalch <bradrydalch@gmail.com>
2018-10-02 16:11:32 -0600

    .

--- web/TeachOneAnotherW03/results.php
+++ web/TeachOneAnotherW03/results.php
@@ -2,9 +2,9 @@
 <body>
 
 Welcome <?php echo $_POST["name"]; ?><br>
-Your email address is: <?php echo $_POST["email"]; ?>
-Your major is: <?php echo $_POST["major"];?>
-Your comment is: <?php echo $_POST["comment"];?>
+Your email address is: <?php echo $_POST["email"]; ?><br>
+Your major is: <?php echo $_POST["major"];?><br>
+Your comment is: <?php echo $_POST["comment"];?><br>
 
 </body>
 </html>
