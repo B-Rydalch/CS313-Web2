8b84059ac525ec6aecc2e8801b9c6712275242f7
B-Rydalch <bradrydalch@gmail.com>
2018-10-03 05:30:22 -0600

    .

--- web/shopping-cart.php
+++ web/shopping-cart.php
@@ -12,7 +12,7 @@
     <body >
         <?php require 'header.php' ?>
         <?php require 'shopping-script.php' ?>
-        <h1>Shopping Cart</h1>
+        <h1 class="cart-title">Shopping Cart</h1>
         <div class="shopping-cart">
           <div class="column-labels">
             <label class="product-image">Image</label>
