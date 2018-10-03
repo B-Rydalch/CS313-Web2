4275444e7963391d56c011b407497647a80eddde
B-Rydalch <bradrydalch@gmail.com>
2018-10-02 21:27:37 -0600

    html link

--- web/shopping.php
+++ web/shopping.php
@@ -40,6 +40,7 @@
           </div>
         </div>
         <div>
+          <button><a href="shopping-cart.php">View Shopping Cart</a></button>
         </div>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
         crossorigin="anonymous"></script>
