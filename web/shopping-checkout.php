26aba744b4806924153ddd8d76a64125fe1e4f2b
B-Rydalch <bradrydalch@gmail.com>
2018-10-05 17:40:04 -0600

    dynamic changes

---
+++ web/shopping-checkout.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">   
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <title>CS313</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
+        crossorigin="anonymous">
+        <link rel="stylesheet" href="styles.css">
+        <script src="scripts.js"></script>
+    </head> 
+    <body class="cart-body">
+        <?php require 'header.php' ?>
+        <?php require 'shopping-script.php' ?>
+        <h1 class="cart-title"><u>Checkout</u></h1>
+  </body>
+</html>
