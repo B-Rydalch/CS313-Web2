658fea579846ee306befb8acdfcecd82fa2c5099
B-Rydalch <bradrydalch@gmail.com>
2018-10-06 11:02:27 -0600

    bug fixing

--- web/shopping.php
+++ web/shopping.php
@@ -42,13 +42,7 @@
             <div class='card-body'>
               <h5 class='card-title'>Pokemon Trainer Kit</h5>
               <p class='card-text'>Become a professional Pokemon trainer with this special trainers kit!</p>
-              <!-- <?php 
-                  if ($_SESSION['inventory']['pokemon']['quantity'] == 1) {
-                    echo '<p class='card-text' name=price[] value='24.99'><small class='text-muted'>$24.99</small><button class='btn'>Add Another?</button></p>';
-                  } else {
-                    echo '<p class='card-text' name=price[] value='24.99'><small class='text-muted'>$24.99</small><button class='btn'>Add to cart</button></p>';
-                  }
-              ?> -->
+              <!-- <div> </div>-->
             </div>
           </div>
           <div class='card'>
