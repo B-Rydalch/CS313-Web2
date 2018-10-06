4185e439893a7a5cd2182fee01ff1a4588546f50
B-Rydalch <bradrydalch@gmail.com>
2018-10-06 15:49:37 -0600

    bug fixes

--- web/prj04/shopping.php
+++ web/prj04/shopping.php
@@ -42,7 +42,7 @@
               <h5 class='card-title'>Pokemon Trainer Kit</h5>
               <p class='card-text'>Become a professional Pokemon trainer with this special trainers kit!</p>
                 <?php 
-                  var_dump($_POST['activeSession'])
+                  //var_dump($_POST['activeSession'])
                   if ($_SESSION['inventory']['pokemon']['quantity'] == 1) {
                     echo "<p class='card-text'><small class='text-muted'>$24.99</small><button class='btn'>Add Another?</button></p>";
                   } else {
