c648cdbd9e1def5a7d3d34d62bc7ec6768402042
B-Rydalch <bradrydalch@gmail.com>
2018-10-06 22:11:16 -0600

    php changes

--- web/prj04/shopping.php
+++ web/prj04/shopping.php
@@ -7,9 +7,9 @@
   }
 
   $inventory = array(
-    'pokemon'  => array('name'=>'pokemon', 'img'=>'shopping-items/pokemon1.jpg', 'desc'=>'Become a professional Pokemon trainer with this special trainers kit!','quantity'=> 0, 'price'=> 24.99),
-    'avengers' => array('name'=>'avengers', 'img'=>'shopping-items/avengers.jpg','desc'=>'Join the avengers in epic battles against Loki!','quantity'=> 0,'price'=> 45.99),
-    'ferdinand'=> array('name'=>'ferdinand', 'img'=>'shopping-items/ferdinand.jpg', 'desc'=>'Have fun with Ferdinand and his friends.', 'quantity'=>0, 'price'=>29.99)
+    'pokemon'  => array('name'=>'pokemon-kit', 'img'=>'shopping-items/pokemon1.jpg', 'desc'=>'Become a professional Pokemon trainer with this special trainers kit!','quantity'=> 0, 'price'=> 24.99),
+    'avengers' => array('name'=>'avengers-kit', 'img'=>'shopping-items/avengers.jpg','desc'=>'Join the avengers in epic battles against Loki!','quantity'=> 0,'price'=> 45.99),
+    'ferdinand'=> array('name'=>'ferdinand-kit', 'img'=>'shopping-items/ferdinand.jpg', 'desc'=>'Have fun with Ferdinand and his friends.', 'quantity'=>0, 'price'=>29.99)
   );
   
   if (!isset($_SESSION['activeSession'])) {
@@ -47,7 +47,8 @@
                 <h5 class='card-title'>Pokemon Trainer Kit</h5>
                 <p class='card-text'>Become a professional Pokemon trainer with this special trainers kit!</p>
                 <p class='card-text'>
-                  <small class='text-muted'><?php echo $_SESSION['inventory']['pokemon']['price'];?></small><button class='btn'>Add to cart</button>
+                  <small class='text-muted'><?php echo $_SESSION['inventory']['pokemon']['price'];?></small>
+                  <button class='btn'>Add to cart</button>
                 </p>
               </div>
             </div>
