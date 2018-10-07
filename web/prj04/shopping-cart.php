db7807a219a409b6dd380ae2ed8a2be09ae012cd
B-Rydalch <bradrydalch@gmail.com>
2018-10-06 23:10:20 -0600

    searching for bugs

--- web/prj04/shopping-cart.php
+++ web/prj04/shopping-cart.php
@@ -1,28 +1,28 @@
 <?php    
     session_start();
     
-    if (isset($_POST)) {
-        $_SESSION['inventory'][key($_POST)]['quantity'] = 0;
-    }
+    // if (isset($_POST)) {
+    //     $_SESSION['inventory'][key($_POST)]['quantity'] = 0;
+    // }
     
-    $total = 0;
-    foreach ($_SESSION['inventory'] as $name => $props) {
-        if ($props['quantity'] == 1) {
-            $total += $props['price'];
-        }
-    }
-    $_SESSION['total'] = $total;
+    // $total = 0;
+    // foreach ($_SESSION['inventory'] as $name => $props) {
+    //     if ($props['quantity'] == 1) {
+    //         $total += $props['price'];
+    //     }
+    // }
+    // $_SESSION['total'] = $total;
     
-    function isEmpty($arr) {
-        $val = true;
-        foreach ($arr as $name => $props) {
-            if ($props['quantity'] == 1) {
-                $val = false;
-            }
-        }
+    // function isEmpty($arr) {
+    //     $val = true;
+    //     foreach ($arr as $name => $props) {
+    //         if ($props['quantity'] == 1) {
+    //             $val = false;
+    //         }
+    //     }
     
-        return $val;
-    }
+    //     return $val;
+    // }
 ?>
 
 <!DOCTYPE html>
