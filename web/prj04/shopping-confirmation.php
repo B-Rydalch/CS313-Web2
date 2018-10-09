fae27bf99d8edd69f4852fb3f204d5d0f7d84df6
B-Rydalch <bradrydalch@gmail.com>
2018-10-08 22:10:40 -0600

    fixing links

--- web/prj04/shopping-confirmation.php
+++ web/prj04/shopping-confirmation.php
@@ -17,7 +17,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
     <link rel="shortcut icon" type="image/png" href="https://cdn1.iconfinder.com/data/icons/outlined-medieval-icons-pack/200/magic_staff-512.png">
-    <title>Ozzy's Wizard Shop</title>
+    <title>CS313 Shopping cart</title>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU"
         crossorigin="anonymous">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
