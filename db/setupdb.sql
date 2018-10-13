ab2ba830f11266703190ef008b823a2ff59ab093
B-Rydalch <bradrydalch@gmail.com>
2018-10-13 01:26:24 -0600

    added database and tables

---
+++ db/setupdb.sql
@@ -0,0 +1,21 @@
+
+
+CREATE TABLE user (
+	id 			 SERIAL 		PRIMARY KEY,
+	username 	 VARCHAR(50)	UNIQUE	NOT NULL, 
+    password 	 VARCHAR(50) 	NOT NULL
+);
+
+CREATE TABLE inventory(
+   id 		    SERIAL 		NOT NULL PRIMARY KEY,
+   item_name    VARCHAR(25) NOT NULL,
+   quantity     INTEGER 	NOT NULL, 
+   best_by		DATE                , 
+   parishable   BOOLEAN 	NOT NULL, 
+   category     VARCHAR(15) NOT NULL,
+   storage_type VARCHAR(15) NOT NULL, 
+   user_id	 	INT			NOT NULL REFERENCES user(id)
+);
+
+
+
---
+++ db/ta04.sql
@@ -0,0 +1,28 @@
+/* Tips and tricks 
+	\dt = show table
+    \d = displays table 
+*/ 
+
+CREATE TABLE users (
+	id 			 SERIAL 		PRIMARY KEY
+    , username 	 VARCHAR(50)	UNIQUE	NOT NULL 
+    , password 	 VARCHAR(50) 	NOT NULL
+); 
+CREATE TABLE speakers (
+	id 			 SERIAL 		PRIMARY KEY
+    , name		 VARCHAR(50)	   
+    
+);
+CREATE TABLE sessions (
+	id 			 SERIAL 		PRIMARY KEY 
+    , month		 SMALLINT		NOT NULL 
+    , year		 SMALLINT		NOT NULL
+); 
+CREATE TABLE notes (
+	id			 SERIAL 		PRIMARY KEY
+    , content	 TEXT			NOT NULL 
+    , date		 DATE
+    , user_id	 INT			NOT NULL REFERENCES users(id)
+    , speaker_id INT			NOT NULL REFERENCES speakers(id)
+    
+);
