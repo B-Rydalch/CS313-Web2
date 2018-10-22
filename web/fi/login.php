6995a91d6cf6510ad3b2730e7e3cd466164462d1
B-Rydalch <bradrydalch@gmail.com>
2018-10-21 22:34:52 -0600

    db changes

--- web/fi/login.php
+++ web/fi/login.php
@@ -75,7 +75,7 @@
             <!-- <img class="profile-img-card" src="//lh3.googleusercontent.com/-6V8xOA6M7BA/AAAAAAAAAAI/AAAAAAAAAAA/rzlHcD0KYwo/photo.jpg?sz=120" alt="" /> -->
             <img id="profile-img" class="profile-img-card" src="//ssl.gstatic.com/accounts/ui/avatar_2x.png" />
             <p id="profile-name" class="profile-name-card"></p>
-            <form class="form-signin">
+            <form class="form-signin"action='' metod="POST">
                 <span id="reauth-email" class="reauth-email"></span>
                 <input type='text' id="input-user" class="form-control" placeholder="User Name" required autofocus>
                 <input type="password" id="input-password" class="form-control" placeholder="Password" required>
