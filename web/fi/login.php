ba630dac4c9256cbc00a12fae1440e8436bdf50c
B-Rydalch <bradrydalch@gmail.com>
2018-10-21 22:57:32 -0600

    db connection

--- web/fi/login.php
+++ web/fi/login.php
@@ -13,7 +13,7 @@
             $dbPassword = $dbopts["pass"];
             $dbName = ltrim($dbopts["path"],'/');
             $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            //$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         }
         catch (PDOException $ex) {
             if (!$production) {
@@ -48,10 +48,10 @@
         }
     }
 
-    // if (isset($_POST)) {
-    //     $db = get_db();
-    //     cheflogin($db);
-    // }
+    if (isset($_POST)) {
+        $db = get_db();
+        cheflogin($db);
+    }
 ?>
 
 <!DOCTYPE HTML>
@@ -73,10 +73,9 @@
 <body>
 <div class="container">
         <div class="card card-container">
-            <!-- <img class="profile-img-card" src="//lh3.googleusercontent.com/-6V8xOA6M7BA/AAAAAAAAAAI/AAAAAAAAAAA/rzlHcD0KYwo/photo.jpg?sz=120" alt="" /> -->
             <img id="profile-img" class="profile-img-card" src="//ssl.gstatic.com/accounts/ui/avatar_2x.png" />
             <p id="profile-name" class="profile-name-card"></p>
-            <form class="form-signin"action='' metod="POST">
+            <form class="form-signin" action="" metod="POST">
                 <span id="reauth-email" class="reauth-email"></span>
                 <input type='text' id="input-user" class="form-control" placeholder="User Name" required autofocus>
                 <input type="password" id="input-password" class="form-control" placeholder="Password" required>
