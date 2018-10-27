dec3eb5116b6c3badc050fda9c649d1448fcee58
B-Rydalch <bradrydalch@gmail.com>
2018-10-26 20:05:12 -0600

    bug fixing

--- web/fi/insert-item.php
+++ web/fi/insert-item.php
@@ -1,4 +1,11 @@
 <?php
+
+    if (!isset($_GET['chef_id']))
+    {
+    	die("Error, chef id not specified...");
+    }
+
+
     // variables 
     $item = htmlspecialchars($_POST['iname']);
     $quantity = htmlspecialchars($_POST['quantity']);
@@ -6,6 +13,8 @@
     $peri = htmlspecialchars($_POST['perishable']);
     $department = htmlspecialchars($_POST['category']);
     $wrap = htmlspecialchars($_POST['storage']);
+    $chefid = htmlspecialchars($_GET['chef_id']);
+
 
 
     // connect db
@@ -13,7 +22,7 @@
     $db = get_db();
 
     $stmt = $db->prepare('INSERT INTO inventory(item_name, quantity, best_by, perishable, category, storage_type, chef_id ) 
-                        VALUES (:purchase, :amount, :eatby, :parish, :brand, :wrapping);');
+                        VALUES (:purchase, :amount, :eatby, :parish, :brand, :wrapping, :cook);');
     
     // binding area 
     $stmt->bindValue(':purchase', $item, PDO::PARAM_INT);
@@ -22,6 +31,8 @@
     $stmt->bindValue(':parish', $peri, PDO::PARAM_BOOL);
     $stmt->bindValue(':brand', $department, PDO::PARAM_STR);
     $stmt->bindValue(':wrapping', $wrap, PDO::PARAM_STR);
+    $stmt->bindValue(':cook', $chefid, PDO::PARAM_INT);
+
     
     $stmt->execute();
     $new_page = "index.php";
