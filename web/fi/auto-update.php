838f0c5ddab7ed3c2fd202e860258a613b1ccd11
B-Rydalch <bradrydalch@gmail.com>
2018-11-04 14:05:50 -0700

    adding error handling to insert item

--- web/fi/auto-update.php
+++ web/fi/auto-update.php
@@ -1,5 +1,5 @@
 <?php
-    session_start();
+    //session_start();
     require('dbconnection.php');
     $db = connect_db();
 
@@ -8,7 +8,33 @@
     $rquantity = htmlspecialchars($_POST['ramount']);
     $chefid = 1; 
     
-    echo "variables initialized<br>";
+    function delete_row($db) {
+        $stmt = $db->prepare("DELETE FROM inventory WHERE item_name = :rit AND id = :rid");
+        $stmt->bindValue(':rit', $row['item_name'], PDO::PARAM_STR);
+        $stmt->bindValue(':rid', $row['id'], PDO::PARAM_INT);
+        $stmt->execute();
+    }
+
+    function update_inventory($db){
+        $stmt = $db->prepare("UPDATE inventory SET quantity = (:ramt - :rqty) 
+                                WHERE id = :rid AND item_name = rit;");
+        $stmt->bindValue(':ramt', $row['quantity'], PDO::PARAM_INT);
+        $stmt->bindValue(':rqty', $rquantity, PDO::PARAM_INT);
+        $stmt->bindValue(':rid', $row['id'], PDO::PARAM_INT);
+        $stmt->bindValue(':rit', $ritem, PDO::PARAM_STR);
+        echo $stmt;
+        $stmt->execute();  
+    }
+
+    function update_shoppinglist($db){
+        $stmt = $db->prepare('INSERT INTO shopping (item_name, quantity, category, chef_id ) 
+                                VALUES (:iname, :iqty, :ict, :cook);');
+        $stmt = $db->bindValue(':iname', $ritem, PDO::PARAM_STR);
+        $stmt = $db->bindValue(':iqty', $rquantity, PDO::PARAM_INT);
+        $stmt = $db->bindValue(':ict', $row['category'], PDO::PARAM_STR);
+        $stmt = $db->bindValue(':cook', $chefid, PDO::PARAM_INT);
+        $stmt->execute();
+    }
 
     // grab what the user is wanting to remove from database and confirm quantity is there. 
     $stmt = $db->prepare("SELECT id, item_name, quantity, category FROM inventory WHERE item_name = :it");
@@ -20,63 +46,15 @@
     if (($row['quantity'] - $rquantity) == 0) {
             
         // delete row 
-        $stmt = $db->prepare("DELETE FROM inventory WHERE item_name = :rit AND id = :rid");
-        $stmt->bindValue(':rit', $row['item_name'], PDO::PARAM_STR);
-        $stmt->bindValue(':rid', $row['id'], PDO::PARAM_INT);
-        $stmt->execute();
+        delete_row($db);
 
     } else if(($row['quantity'] - $rquantity) > 0) {
 
-        // sql statements
-        $sql = $db->prepare("UPDATE inventory SET quantity = (:ramt - :rqty) 
-                                WHERE id = :rid AND item_name = rit;");
-        $sql2= $db->prepare('INSERT INTO shopping (item_name, quantity, category, chef_id ) 
-                                    VALUES (:iname, :iqty, :ict, :cook);');
-
-
-        // binding variables 
-        $sql  = $db->bindValue(':ramt', $row['quantity'], PDO::PARAM_INT);
-        $sql  = $db->bindValue(':rqty', $rquantity, PDO::PARAM_INT);
-        $sql  = $db->bindValue(':rid', $row['id'], PDO::PARAM_INT);
-        $sql  = $db->bindValue(':rit', $ritem, PDO::PARAM_STR);
-        $sql2 = $db->bindValue(':iname', $ritem, PDO::PARAM_STR);
-        $sql2 = $db->bindValue(':iqty', $rquantity, PDO::PARAM_INT);
-        $sql2 = $db->bindValue(':ict', $row['category'], PDO::PARAM_STR);
-        $sql2 = $db->bindValue(':cook', $chefid, PDO::PARAM_INT);
-
-        $sql->execut();
-        $sql2->execute();
-
-
-
         // update inventory
-        // $stmt = $db->prepare("UPDATE inventory SET quantity = (:ramt - :rqty) 
-        //                         WHERE id = :rid AND item_name = rit;");
-        // $stmt->bindValue(':ramt', $row['quantity'], PDO::PARAM_INT);
-        // $stmt->bindValue(':rqty', $rquantity, PDO::PARAM_INT);
-        // $stmt->bindValue(':rid', $row['id'], PDO::PARAM_INT);
-        // $stmt->bindValue(':rit', $ritem, PDO::PARAM_STR);
-        // echo $stmt;
-        // $stmt->execute();  
-        
-        //echo "stmt executed";
+        update_inventory($db);
 
         // insert into shopping list
-        // $sql = $db->prepare('INSERT INTO shopping (item_name, quantity, category, chef_id ) 
-        //                         VALUES (:iname, :iqty, :ict, :cook);');
-        // $sql = $db->bindValue(':iname', $ritem, PDO::PARAM_STR);
-        // $sql = $db->bindValue(':iqty', $rquantity, PDO::PARAM_INT);
-        // $sql = $db->bindValue(':ict', $row['category'], PDO::PARAM_STR);
-        // $sql = $db->bindValue(':cook', $chefid, PDO::PARAM_INT);
-        // $sql->execute();
-
-        //echo "sql executed";
-
-        // return to page
-        // $new_page = "index.php";
-        // header("Location: $new_page");
-        
-        // die();
+        update_shoppinglist($db);
 
     } else{
         // ERROR handler
--- web/fi/insert-item.php
+++ web/fi/insert-item.php
@@ -5,15 +5,11 @@
     // 	die("Error, chef id not specified...");
     // }
 
-    // variables 
-    $item = htmlspecialchars($_POST['iname']);
-    $quantity = htmlspecialchars($_POST['quantity']);
-    $freshness = htmlspecialchars($_POST['bestby']);
-    $peri = htmlspecialchars($_POST['perishable']);  
-    $department = htmlspecialchars($_POST['category']);
-    $wrap = htmlspecialchars($_POST['storage']);
-    //$chefid = htmlspecialchars($_GET['chef_id']);
-    $chefid = 1;   
+    // connect db
+    require('dbconnection.php');
+    $db = connect_db();
+
+
 
     // convert checkbox value to boolean
     if ($peri != 'on') {
@@ -22,25 +18,56 @@
         $peri = true;
     }
 
-    // connect db
-    require('dbconnection.php');
-    $db = connect_db();
+    function additem($db) {
+        // variables 
+        $item = htmlspecialchars($_POST['iname']);
+        $quantity = htmlspecialchars($_POST['quantity']);
+        $freshness = htmlspecialchars($_POST['bestby']);
+        $peri = htmlspecialchars($_POST['perishable']);  
+        $department = htmlspecialchars($_POST['category']);
+        $wrap = htmlspecialchars($_POST['storage']);
+        //$chefid = htmlspecialchars($_GET['chef_id']);
+        $chefid = 1;   
+        
+        try {
 
-    $stmt = $db->prepare('INSERT INTO inventory(item_name, quantity, best_by, perishable, category, storage_type, chef_id ) 
-                        VALUES (:purchase, :amount, :eatby, :parish, :brand, :wrapping, :cook);');
+            $stmt = $db->prepare('INSERT INTO inventory(item_name, quantity, best_by, perishable, category, storage_type, chef_id ) 
+            VALUES (:purchase, :amount, :eatby, :parish, :brand, :wrapping, :cook);');
     
-    // binding area 
-    $stmt->bindValue(':purchase', $item, PDO::PARAM_STR);
-    $stmt->bindValue(':amount', $quantity, PDO::PARAM_INT);
-    $stmt->bindValue(':eatby', $freshness, PDO::PARAM_STR);
-    $stmt->bindValue(':parish', $peri, PDO::PARAM_BOOL);
-    $stmt->bindValue(':brand', $department, PDO::PARAM_STR);
-    $stmt->bindValue(':wrapping', $wrap, PDO::PARAM_STR);
-    $stmt->bindValue(':cook', $chefid, PDO::PARAM_INT);
+            // binding area 
+            $stmt->bindValue(':purchase', $item, PDO::PARAM_STR);
+            $stmt->bindValue(':amount', $quantity, PDO::PARAM_INT);
+            $stmt->bindValue(':eatby', $freshness, PDO::PARAM_STR);
+            $stmt->bindValue(':parish', $peri, PDO::PARAM_BOOL);
+            $stmt->bindValue(':brand', $department, PDO::PARAM_STR);
+            $stmt->bindValue(':wrapping', $wrap, PDO::PARAM_STR);
+            $stmt->bindValue(':cook', $chefid, PDO::PARAM_INT);
     
-    $stmt->execute();
+            $stmt->execute();
+
+            // index.php?id=$pId
+            $new_page = "index.php";
+            echo $page;
+            header("Location: $new_page");
+            die();
+
+        } catch(PDOException $ex) {
+            die();
+        }
+
+    }
+
+    additem($db);
     $new_page = "index.php";
     header("Location: $new_page");
     die();
+
+    // if (isset($_POST['chefid'])) {
+    //     $db = connect_db();
+    //     additem($db);
+    // } else {
+    //     header('Location: index.php');
+    //     die();
+    // }
 
 ?>
