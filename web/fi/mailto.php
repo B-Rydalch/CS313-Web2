e966d117de07669e1a68a3e53f5544d93e9f511c
B-Rydalch <bradrydalch@gmail.com>
2018-11-06 10:22:17 -0700

    fixing headers

--- web/fi/mailto.php
+++ web/fi/mailto.php
@@ -32,7 +32,7 @@
 
     $headers = "From: ryd11002@byui.edu \r\n";
     //$headers .= "Reply-To: ". strip_tags($_POST['req-email']) . "\r\n";
-    $headers .= "CC: \r\n";
+    // $headers .= "CC: \r\n";
     $headers .= "MIME-Version: 1.0\r\n";
     $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
