0a61f086ae3401d9b0b945ab478bccb3757ef67f
B-Rydalch <bradrydalch@gmail.com>
2018-11-05 17:37:08 -0700

    testing

--- web/fi/insert-item.php
+++ web/fi/insert-item.php
@@ -62,7 +62,7 @@
 
         try {
 
-            if ($table == "inventory") { echo $tim; 
+            if ($table == "inventory") { echo gettype($tim); 
                 $stmt = $db->prepare('select exists (select 1  from inventory where item_name = :it LIMIT 1);');
             } else {
                 $stmt = $db->prepare('select exists (select 1  from shopping where item_name = :it LIMIT 1);');
