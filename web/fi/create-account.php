dad540f48c74236ec97b8cb4868bd45d7a62e38f
B-Rydalch <bradrydalch@gmail.com>
2018-11-05 20:19:38 -0700

    undo css changes

--- web/fi/create-account.php
+++ web/fi/create-account.php
@@ -33,9 +33,9 @@
 
 </head>
 <body>
-    <div>
-        <div>
-        <div>
+    <div class="container">
+        <div class="row centered-form">
+        <div class="col-xs-12 col-sm-8 col-md-4 col-sm-offset-2 col-md-offset-4">
         	<div class="panel panel-default">
         		<div class="panel-heading">
 			    		<h3 class="panel-title">Please sign up for Food Inventory  <small><br>Everyone likes FI Becuse It's free!</small></h3>
