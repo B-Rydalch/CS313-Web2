91e4c4c9f84b2e31f18c06b00828771fe96fc1d1
B-Rydalch <bradrydalch@gmail.com>
2018-11-05 21:35:57 -0700

    testing logout

--- web/fi/index.php
+++ web/fi/index.php
@@ -120,7 +120,7 @@
                                             <td><a class="btn" onclick=togglex()>Add New Item</a></td>
                                             <td><a class="btn" id="rbtn" onclick=toggley()>Remove Item</a></td>
                                             <td><a class="btn" id="shoppinglist" href="shopping-list.php">Shopping List</a></td>
-                                            <td><a class="btn" href="logout.php">Log Out</a></td>
+                                            <td><a class='nav-link' href='login.php'><?php session_unset(); ?>Log Out</a></td>
                                         </tr>    
                                     </tbody>
                                 </table>
