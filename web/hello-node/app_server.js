98036452be07a55ee530c5333de64b3f43966558
Brad Rydalch <bradrydalch@gmail.com>
2018-11-13 13:54:58 -0700

    forgot to upload to git

--- web/hello-node/app_server.js
+++ web/hello-node/app_server.js
@@ -10,14 +10,8 @@ http.createServer((request, response) => {
         response.writeHead(200, {'Content-Type': 'text/html'});
 
         response.write('<h1>Welcome to the Node Server main page!</h1>');
-    } else if (app_url.pathname === '/getData') {
-        response.writeHead(200, {'Content-Type': 'application/json'});
-
-        response.write(JSON.stringify({
-            'name': 'Professor Burton',
-            'class': 'CS313'
-        }));
-    } else {
+        response.write('<p>I only had to wipe my hardrive and reinstall windows to get node working on my machine</p>');
+    }  else {
         response.writeHead(404, {'Content-Type': 'text/html'});
 
         response.write('<h1>Page not found!</h1>');
